8268540c34050f86e454060d13ac339c6f966eeb
Keller Pascal <pascalkeller99@gmail.com>
2021-03-02 11:05:01 +0100

    finalisation du dossier EVAL 03

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 $(document).ready(() => {
 
-    function JoueurSelected (joueur) {
+    /** Variable pour calcul du score **/
 
-        if (joueur == 1)
-        {
-            
-            $("#player1").css('border','solid');            
-            $("#player1").css('border-radius','5px');
-            $("#player1").css('padding','2%');
-            $("#player1").css('border-color','#000000');
-            console.log('changement joueur 1');
-            
+    let player = 1;
+    let scoreTotalPlayer1 = 0;
+    let scoreTotalPlayer2 = 0;
+    let scoreCurrentPlayer1 = 0;
+    let scoreCurrentPlayer2 = 0;
+
+
+    /** Fonction de selection du joueur **/
+    function JoueurSelected(joueur) {
+
+        if (joueur == 1) {
+
+            $("#player1").css('border', 'solid');
+            $("#player1").css('border-radius', '5px');
+            $("#player1").css('border-color', '#000000');
+            $("#player2").css('border', 'none');
         }
 
+        if (joueur == 2) {
+
+            $("#player2").css('border', 'solid');
+            $("#player2").css('border-radius', '5px');
+            $("#player2").css('border-color', '#000000');
+            $("#player1").css('border', 'none');
+        }
     }
 
-
-
+    /** Fonction pour animer le dé **/
     function AnimationDe(face) {
         if (face == 0) {
             $("#imageDe").attr('src', '/images/face-1.png').delay(1000);
@@ -36,41 +49,103 @@ $(document).ready(() => {
         else if (face == 5) {
             $("#imageDe").attr('src', '/images/face-6.png').delay(1000);
         }
-
-
     }
 
+    /** Fonction pour chiffre aléatoire **/
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
     }
 
+    /** Fonction nouvelle partie **/
     function NewGame() {
-        alert('Lance un nouveau jeux')
+        alert('Lancement d\'une nouvelle partie')
+        scoreTotalPlayer1 = 0;
+        scoreTotalPlayer2 = 0;
+        scoreCurrentPlayer1 = 0;
+        scoreCurrentPlayer2 = 0;
+        $("#currentScore1").text(scoreCurrentPlayer1);
+        $("#currentScore2").text(scoreCurrentPlayer2);
+        $("#scorePlayer1").text(scoreTotalPlayer1);
+        $("#scorePlayer2").text(scoreTotalPlayer2);
     }
 
+    /** Fonction lancer de dé **/
     function RollDice() {
-        /**alert('RollDice')**/
-        let rollResult;
 
+        let rollResult;
         rollResult = getRandomInt(6);
         AnimationDe(rollResult);
 
+        if (player == 1) {
+            scoreCurrentPlayer1 = scoreCurrentPlayer1 + rollResult + 1;
+            $("#currentScore1").text(scoreCurrentPlayer1);
+
+            if (rollResult == 0) {
+                scoreCurrentPlayer1 = 0;
+                $("#currentScore1").text(scoreCurrentPlayer1);
+                alert('Vous avez fait un 1, changement de joueur, score courant mit à 0');
+                Hold();
+            }
+        }
+        else if (player == 2) {
+
+            scoreCurrentPlayer2 = scoreCurrentPlayer2 + rollResult + 1;
+            $("#currentScore2").text(scoreCurrentPlayer2);
+
+            if (rollResult == 0) {
+                scoreCurrentPlayer2 = 0;
+                $("#currentScore2").text(scoreCurrentPlayer2);
+                alert('Vous avez fait un 1, changement de joueur, score courant mit à 0');
+                Hold();
+            }
+        }
+        Winner();
     }
 
+    /** Fonction fin de tour changement de joueur **/
     function Hold() {
-        JoueurSelected(1);
+        if (player == 1) {
+            player = 2;
+            scoreTotalPlayer1 = scoreTotalPlayer1 + scoreCurrentPlayer1;
+            scoreCurrentPlayer1 = 0;
+            $("#scorePlayer1").text(scoreTotalPlayer1);
+            $("#currentScore1").text(scoreCurrentPlayer1);
+        }
+        else if (player == 2) {
+            player = 1;
+            scoreTotalPlayer2 = scoreTotalPlayer2 + scoreCurrentPlayer2;
+            scoreCurrentPlayer2 = 0;
+            $("#scorePlayer2").text(scoreTotalPlayer2);
+            $("#currentScore2").text(scoreCurrentPlayer2);
+        }
+        JoueurSelected(player);
+    }
+
+    /** Fonction vérification gagnant **/
+    function Winner() {
+        let score1 = scoreTotalPlayer1 + scoreCurrentPlayer1;
+        let score2 = scoreTotalPlayer2 + scoreCurrentPlayer2;
+
+        if (score1 >= 20) {
+            console.log('Score joueur 1 :' + scoreTotalPlayer1);
+            alert('Le joueur 1 à gagné !');
+            NewGame();
+        }
+        if (score2 >= 20) {
+            console.log('Score joueur 1 :' + scoreTotalPlayer2);
+            alert('Le joueur 2 à gagné !');
+            NewGame();
+        }
+
     }
 
 
-
-
+    /** vérification d'évenements **/
     $('#btnNewGame').click(function () { NewGame(); })
     $('#btnRollDice').click(function () { RollDice(); });
     $('#btnHold').click(function () { Hold(); })
 
-    
 
-
-
+    JoueurSelected(player);
 
 })
